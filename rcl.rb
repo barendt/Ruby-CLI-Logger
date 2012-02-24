31cf45cedf07ccae1ae868d1e52e9a1e1f789300
Greg Barendt <github@gregbarendt.com>
2012-02-24 09:06:41 -0500

    Deleted an quick hack test file.

--- rcl.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require './rcl/dispatcher.rb'
-
-begin
-  Dispatcher.new('log some stuff').dispatch
-rescue DispatcherActionNotFoundError
-  puts "Sorry, that's an invalid action."
-end

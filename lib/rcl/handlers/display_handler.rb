87ccdce52a79f151917b043c58f2a49ac5cd3ab1
Greg Barendt <github@gregbarendt.com>
2012-02-25 17:06:48 -0500

    DisplayHandler actually displays items now.

--- lib/rcl/handlers/display_handler.rb
+++ lib/rcl/handlers/display_handler.rb
@@ -1,6 +1,16 @@
+require 'rcl/item'
+
 class DisplayHandler
 
   def initialize(args)
+  end
+
+  def handle
+    puts "ID\tText\n"
+    puts "--\t----\n"
+    Item.all.each do |i|
+      puts "#{i.id}\t#{i.entry}\n"
+    end
   end
 
 end

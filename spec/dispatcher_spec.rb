2604577cee286ef5c5791b96f2dcc4bc7b402798
Greg Barendt <github@gregbarendt.com>
2012-02-27 21:57:53 -0500

    Test works. Hurray

--- spec/dispatcher_spec.rb
+++ spec/dispatcher_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Dispatcher do
 
   it 'errors on actions that are invalid' do
-    lambda {Dispatcher.new(["!log", "something"])}.should raise_error(DispatcherActionNotFoundError)
+    opts = {:action => 'll'}
+    lambda {Dispatcher.new(opts, "something")}.should raise_error(DispatcherActionNotFoundError)
   end
 
 end

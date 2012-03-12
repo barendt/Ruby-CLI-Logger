74ef59309cf52d58f2d79cd41d82f7051eb5e99e
Greg Barendt <github@gregbarendt.com>
2012-03-11 23:37:12 -0400

    Fixed the item spec so it now uses an in-memory SQLite database.

--- lib/rcl/item.rb
+++ lib/rcl/item.rb
@@ -3,8 +3,10 @@ require 'sequel'
 
 DB = DbHelper.get_db_connection
 class Item < Sequel::Model
+
   plugin :validation_helpers
   def validate
     validates_presence :entry
   end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'rcl'
 
 RSpec.configure do |config|
-  config.around(:each) do |example|
-    Sequel::DATABASES.first.transaction do
-      example.run
-      raise Sequel::Error::Rollback
-    end
-  end
+#   config.around(:each) do |example|
+#     Sequel::DATABASES.first.transaction do
+#       example.run
+#       raise Sequel::Error::Rollback
+#     end
+#   end
 end

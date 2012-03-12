91e047251f5a7e746a1a4abfc96d435fee1b624d
Greg Barendt <github@gregbarendt.com>
2012-03-11 23:45:30 -0400

    Moved the temp SQLite db creation into spec_helper.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,17 +4,6 @@ describe Item do
 
   context 'creation' do
 
-    # TODO: Find a better way to do this.
-    before(:all) do
-      db = Sequel.sqlite
-      Item.db = db
-      db.create_table :items do
-        primary_key :id
-        Text :entry
-        DateTime :logged_at, :default => Sequel::CURRENT_TIMESTAMP
-      end
-    end
-
     it 'can be created' do
       Item.create(:entry => Time.now.to_i)
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 require 'rcl'
 
 RSpec.configure do |config|
-#   config.around(:each) do |example|
-#     Sequel::DATABASES.first.transaction do
-#       example.run
-#       raise Sequel::Error::Rollback
-#     end
-#   end
+  config.around(:each) do
+    db = Sequel.sqlite
+    Item.db = db
+    db.create_table :items do
+      primary_key :id
+      Text :entry
+      DateTime :logged_at, :default => Sequel::CURRENT_TIMESTAMP
+    end
+  end
 end
